7addd9a7f8d9073c285057bb45b40f91eb840907
toannguyen156 <70114361+toannguyen156@users.noreply.github.com>
2020-11-12 22:25:24 -0800

    Update Paddle.gd
    Limit to 5 ball count

--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 
 const ball_scene = preload("res://MiniScenes/Ball.tscn")
-
+var lifecount = 0;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
@@ -17,7 +17,11 @@ func _physics_process(delta):
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT and event.pressed:
-			var ball = ball_scene.instance()
-			ball.set_position(get_position() - Vector2(0, 16))
-			get_tree().get_root().add_child(ball)
+		
+		
+			if event.button_index == BUTTON_LEFT and event.pressed and  lifecount < 5:
+				var ball = ball_scene.instance()
+				lifecount+=1
+				ball.set_position(get_position() - Vector2(0, 16))
+				get_tree().get_root().add_child(ball)
+			
